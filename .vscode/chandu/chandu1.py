437227a0e2e448094167d059ccb691f568a5dfa9
Chandu <munagavenkatchiranjeevi@gmail.com>
2025-09-05 10:18:43 +0530

    Uploaded Python project

---
+++ .vscode/chandu/chandu1.py
@@ -0,0 +1,46 @@
+# age = int(input("enter your age:"))
+# if not (age>18 and age<5):
+#     print("Monster")
+   
+# else:
+#     print("you are not a human")        
+
+
+# x = 1
+# while x<6:
+#     print("help! I am stuck here!")
+#     x+= 5
+
+# import time
+# for i in range(10,0,-1):
+#     print(i)
+#     time.sleep(1)
+#     if i==5:
+#        print("its 5 sec more")
+# print("Happy New Year!")    
+
+# rows = int(input("Enter the nof rows:"))
+# cols = int(input("Enter the nof cols:"))
+
+# for i in range(rows):
+#     for j in range(cols):
+#         print("*#&",end="")
+#     print()        
+
+
+
+
+# for i in range(3):
+#     for j in range(6,10):
+#         print(i,end="")
+#     print() 
+# print("success")    
+
+
+# phone_num = "123-456-7890"
+# for i in phone_num:
+#     if i=="-":
+#        pass
+#     else:
+#         print(i,end="")
+
